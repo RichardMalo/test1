004ea17dacc5db701b8e59c1dfd90b4a8ff61fef
Richard Malolepszy <86073298+Rolosaregood@users.noreply.github.com>
2023-04-17 20:01:18 -0400

    updated routes

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 @app.route('/data')
 def load_csv_data():
     df = pd.read_csv('data/1950-2021_torn.csv')
-    return df.to_json(orient='records')
+    # Add this line to select specific columns
+    selected_columns = ['yr', 'mag', 'inj', 'fat']  
+    # Update this line to return only the selected columns
+    return df[selected_columns].to_json(orient='records')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
